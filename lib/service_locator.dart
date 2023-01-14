0e03c90c7c1829246e8de9c10e01389e38291e0d
Yom3n <kamilbudnik94@gmail.com>
2023-01-14 12:10:25 +0100

    provided repository to main app

---
+++ lib/service_locator.dart
@@ -0,0 +1,11 @@
+import 'package:get_it/get_it.dart';
+import 'package:notes_repository/notes_repository.dart';
+import 'package:notes_database/notes_database.dart';
+
+final sl = GetIt.instance;
+
+void setup() {
+  sl
+    ..registerSingleton(NotesDatabase())
+    ..registerSingleton(NotesRepository(sl()));
+}
